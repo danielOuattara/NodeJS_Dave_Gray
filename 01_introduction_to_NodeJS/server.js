a8f8e3db36e8b2474d94249aa6e2b0b1a874aa45
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-01-29 17:56:41 +0100

    01 intro

--- 01_introduction_to_NodeJS/server.js
+++ 01_introduction_to_NodeJS/server.js
@@ -1,38 +1,56 @@
 // 1- NodeJS runs on the server or anywhere but on the browser
 
+console.log(
+  "---------------------------------------------------------------- 2",
+);
+
 // 2- the console on the terminal window
 console.log("Hello");
+
+console.log(
+  "---------------------------------------------------------------- 3",
+);
 
 // 3- global object instead of the window object
 console.log("global = ", global);
 
-console.log("----------------------------------------------------------------");
+console.log("------------------------------------------");
 
 console.log("module = ", module);
 
-console.log("----------------------------------------------------------------");
+console.log("------------------------------------------");
 
 // 4- has common core modules instead of ES6 modules
 const os = require("node:os");
 console.log("os = ", os);
 
-console.log("----------------------------------------------------------------");
+console.log("os.type() ) = ", os.type());
+console.log("os.version() = ", os.version());
+console.log("os.homedir() = ", os.homedir());
 
-console.log(__dirname);
-console.log(__filename);
+console.log(
+  "---------------------------------------------------------------- 4",
+);
 
-console.log("----------------------------------------------------------------");
+console.log("__dirname = ", __dirname);
+console.log("__filename = ", __filename);
+
+console.log(
+  "----------------------------------------------------------------- 5",
+);
 
 const path = require("path");
 
 console.log("path = ", path);
 
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
-console.log(path.parse(__filename));
+console.log("path.dirname(__filename) = ", path.dirname(__filename));
+console.log("path.basename(__filename) = ", path.basename(__filename));
+console.log("path.extname(__filename) = ", path.extname(__filename));
+console.log("path.parse(__filename) = ", path.parse(__filename));
 
-console.log("----------------------------------------------------------------");
+console.log(
+  "----------------------------------------------------------------- 6",
+);
 //5 - user custom module
 
 const math = require("./math");
@@ -53,7 +71,9 @@ console.log("sub2 = ", subtract(6, 9));
 console.log("multi2 = ", multiply(6, 9));
 console.log("div2 = ", divide(6, 9));
 
-console.log("----------------------------------------------------------------");
+console.log(
+  "----------------------------------------------------------------- 7",
+);
 
 const fetcher = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -64,7 +84,7 @@ const fetcher = async () => {
 const response1 = fetcher();
 console.log("response1 = ", response1);
 
-console.log("----------------------------------------------------------------");
+console.log("---------------------------------------------------------- 8");
 
 const data = fetch("https://jsonplaceholder.typicode.com/users")
   .then((res) => {
